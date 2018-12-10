2f1c285c8d439ed87d7146f93d14c7350cf47268
mohamadjaved <maj38@njit.edu>
2018-12-09 20:03:29 -0500

    Fixed: Branding link to correctly direct logged in user to their homepage.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
+Route::get('/questions/{question_id}/answers/{answer_id}', 'AnswerController@show')->name('answers.show');
+
+Route::resources([
+    'question' => 'QuestionController',
+]);
