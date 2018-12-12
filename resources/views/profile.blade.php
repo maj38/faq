10f0c8df7b2c2db2a7fd70290b7c15033fc471ad
mohamadjaved <maj38@njit.edu>
2018-12-11 22:38:48 -0500

    Notify user that there profile was updated

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,13 +11,11 @@
                         <span class="font-weight-bold">First Name:</span> {{$profile->fname}}</br>
                         <span class="font-weight-bold">Last Name: </span>{{$profile->lname}}</br>
                         <span class="font-weight-bold">Body: </span>{{$profile->body}}</br>
-                    </div>
-                    <div class="card-footer">
-                        <a class="btn btn-success float-right" href="#">
-                            Edit
-                        </a>
-                    </div>
-
+                        <div class="card-footer">
+                            <a class="btn btn-success float-right" href="{{ route('profile.edit', ['profile_id' => $profile->id,'user_id' => $profile->user->id]) }}">
+                                Edit
+                            </a>
+                        </div>
                 </div>
             </div>
         </div>
