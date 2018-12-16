b2944d479f9fc57b61a94dac101ce9e2e6400889
mohamadjaved <maj38@njit.edu>
2018-12-15 19:30:36 -0500

    Added recaptcha (honeypot) for spam protection to registration form.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,37 +1,34 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\profile;
+use App\Profile;
 use App\User;
-
 class ProfileController extends Controller
-
-
 {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         //
     }
-
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-
     public function create()
     {
         $profile = new Profile();
-        $edit = FALSE;
-        return view('profileForm', ['profile' => $profile,'edit' => $edit  ]);
+        $edit= false;
+        return view('profileForm',['profile' => $profile,'edit' => $edit ]);
     }
 
     /**
@@ -43,21 +40,21 @@ class ProfileController extends Controller
     public function store(Request $request)
     {
         $input = $request->validate([
-            'fname' => 'required',
-            'lname' => 'required',
-            'body' => 'required',
+            'fname'=> 'required',
+            'lname'=> 'required',
+            'body'=> 'required'
         ], [
+
             'fname.required' => ' First is required',
             'lname.required' => ' Last is required',
             'body.required' => ' Body is required',
         ]);
-        $input = request()->all();
+        $input = $request->all();
         $profile = new Profile($input);
         $profile->user()->associate(Auth::user());
         $profile->save();
         return redirect()->route('home')->with('message', 'Profile Created');
     }
-
     /**
      * Display the specified resource.
      *
@@ -70,14 +67,13 @@ class ProfileController extends Controller
         $profile = $user->profile;
         return view('profile')->with('profile', $profile);
     }
-
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($user, $profile)
+    public function edit($user,$profile)
     {
         $user = User::find($user);
         $profile = $user->profile;
@@ -91,7 +87,7 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,  $user, $profile)
+    public function update(Request $request,$user,$profile)
     {
         $input = $request->validate([
             'fname' => 'required',
@@ -107,8 +103,6 @@ class ProfileController extends Controller
         $profile->save();
         return redirect()->route('home')->with('message', 'Updated Profile');
     }
-
-
     /**
      * Remove the specified resource from storage.
      *
